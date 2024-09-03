c18b113d6c8b17332beed52ac5386833bb371c63
Laynny123 <00001098280246sp@al.educacao.sp.gov.br>
2024-09-03 18:09:51 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const avanca = document.querySelectorA11('.btn-próxima');
+
+avanca.forEach(button => {
+  button.addEventListener('click',function(){
+    const atual = document.equerySelector(".ativo");
+    const proximo passo = 'passo-' + this.this .getAttribute('data-proximo');
+
+    atual.classList.remove("ativo);
+    document.getElementById(proximoPasso).classList.add("ativo");
+  }) 
+})
